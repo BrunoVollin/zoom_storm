316c1fe56c633aacc3ff204ae80db8736dd41e0b
Bruno <bruno.3av@gmail.com>
2026-06-10 06:24:55 -0300

    chore: remove .next directory from version control

--- apps/web/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"hHTRxfZYjVNv/fUZx+woOat63XgJSNnZJxv2qJ/XNG4=\"\n}"
--- apps/web/.next/static/chunks/apps_web_src_app_page_tsx_1d0f1488._.js
+++
@@ -1,5 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-  script: typeof document === "object" ? document.currentScript : undefined,
-  chunks: ["static/chunks/apps_web_bd5e20b6._.js"],
-  source: "dynamic",
-});
